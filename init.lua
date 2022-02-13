56eb02fd17ed4a594d3e954195486b98bcb6b97c
Niden <80420845+Niden1@users.noreply.github.com>
2022-02-13 16:19:58 -0300

    Enable and Disable command

--- init.lua
+++ init.lua
@@ -1,16 +1,66 @@
-minetest.register_on_dieplayer(function(player)
-    local pname = player:get_player_name()
-    local pos = player:getpos()
-    pos.x = math.floor(pos.x)
-    pos.y = math.floor(pos.y)
-    pos.z = math.floor(pos.z)
+activate = true
 
-    minetest.chat_send_player(pname, core.get_color_escape_sequence("#FF0000") .."*** Server - Last Dead At: " .. pos.x .. ", " .. pos.y .. ", " .. pos.z )
+minetest.register_chatcommand(
+    "death",
+    {
+        description = "Enable or disable death coordinates",
+        params = "<true> | <false>",
+        privs = {ban = true},
+        func = function(name, param)
+            if param == "true" then
+                activate = true
+                minetest.chat_send_all(minetest.colorize("#00FF00", "*** [Server]: Death coordinates are enabled!"))
 
-		music = minetest.sound_play("die",
-{  loop = false,
-   gain = 1.0,
-		})
+                music =
+                    minetest.sound_play(
+                    "notify",
+                    {
+                        loop = false,
+                        gain = 1.0
+                    }
+                )
+            end
 
+            if param == "false" then
+                activate = false
 
-end)
+                minetest.chat_send_all(minetest.colorize("#FF0000", "*** [Server]: Death coordinates are disabled!"))
+
+                music =
+                    minetest.sound_play(
+                    "notify",
+                    {
+                        loop = false,
+                        gain = 1.0
+                    }
+                )
+            end
+        end
+    }
+)
+
+minetest.register_on_dieplayer(
+    function(player)
+        if activate == true then
+            local pname = player:get_player_name()
+            local pos = player:getpos()
+            pos.x = math.floor(pos.x)
+            pos.y = math.floor(pos.y)
+            pos.z = math.floor(pos.z)
+            minetest.chat_send_player(
+                pname,
+                core.get_color_escape_sequence("#FF0000") ..
+                    "*** [Server] - Last Dead At: " .. pos.x .. ", " .. pos.y .. ", " .. pos.z
+            )
+
+            music =
+                minetest.sound_play(
+                "die",
+                {
+                    loop = false,
+                    gain = 1.0
+                }
+            )
+        end
+    end
+)
